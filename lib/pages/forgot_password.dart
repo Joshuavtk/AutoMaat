3cdff5fb329c865dba49c74bad7fd7a809eeba32
Joshuavtk <joshuavtk@hotmail.com>
2025-01-10 08:52:47 +0100

    Update Register and password forgot UI

--- lib/pages/forgot_password.dart
+++ lib/pages/forgot_password.dart
@@ -1,14 +1,5 @@
-import 'dart:ffi';
-
 import 'package:auto_maat/pages/login.dart';
-import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-
-import 'package:auto_maat/ui/button.dart';
-
-import 'dart:async';
-import 'dart:convert';
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
@@ -25,25 +16,49 @@ class _ForgotPasswordPageState extends State<ForgotPasswordScreen> {
     return MaterialApp(
         home: Scaffold(
             body: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const Image(
-                        image: AssetImage('images/splash_screen.png'),
-                        height: 50,
-                      ),
-                      const Text("Forgot password"),
-                      const Text("Type in your e-mail so we can send you the recovery code."),
-                      TextField(
-                        controller: _emailFieldController,
-                        decoration: const InputDecoration(hintText: 'Email'),
-                      ),
-                      TextButton(onPressed: () => Navigator.pop(context),
-                          child: const Text('Back to login')),
-                      TextButton(
-                          onPressed: () => Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => const LoginScreen())),
-                          child: const Text('Send code')),
-                    ]))));
+                child: Container(
+      padding: const EdgeInsets.all(15),
+      margin: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Theme.of(context).colorScheme.tertiaryContainer,
+      ),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Image(
+              image: AssetImage('images/splash_screen.png'),
+              height: 50,
+            ),
+            const SizedBox(height: 20.0),
+            const Text(
+              "Forgot password",
+              style: TextStyle(fontSize: 24),
+              textAlign: TextAlign.end,
+            ),
+            const Text("Type in your e-mail so we can send you the recovery code."),
+            const SizedBox(height: 20.0),
+            TextField(
+              controller: _emailFieldController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Row(
+              children: [
+                TextButton(onPressed: () => Navigator.pop(context), child: const Text('Back to login')),
+                const Spacer(),
+                ElevatedButton(
+                    onPressed: () =>
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginScreen())),
+                    child: const Text('Send code')),
+              ],
+            ),
+          ]),
+    ))));
   }
 }
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,15 +1,5 @@
-import 'dart:ffi';
-
 import 'package:auto_maat/pages/home.dart';
-import 'package:auto_maat/pages/login.dart';
-import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-
-import 'package:auto_maat/ui/button.dart';
-
-import 'dart:async';
-import 'dart:convert';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -21,53 +11,80 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterPageState extends State<RegisterScreen> {
   final TextEditingController _nameFieldController = TextEditingController();
   final TextEditingController _emailFieldController = TextEditingController();
-  final TextEditingController _passwordFieldController =
-      TextEditingController();
-  final TextEditingController _passwordRepeatFieldController =
-      TextEditingController();
+  final TextEditingController _passwordFieldController = TextEditingController();
+  final TextEditingController _passwordRepeatFieldController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
             body: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-          const Image(
-            image: AssetImage('images/splash_screen.png'),
-            height: 50,
-          ),
-          const Text("Create AutoMaat-account"),
-          const Text("Register your account"),
-          TextField(
-            controller: _nameFieldController,
-            decoration: const InputDecoration(hintText: 'Name'),
-          ),
-          TextField(
-            controller: _emailFieldController,
-            decoration: const InputDecoration(hintText: 'Email'),
-          ),
-          TextField(
-            controller: _passwordFieldController,
-            decoration: const InputDecoration(hintText: 'Password'),
-          ),
-          TextField(
-            controller: _passwordRepeatFieldController,
-            decoration: const InputDecoration(hintText: 'Repeat password'),
-          ),
-          Checkbox(
-            value: false,
-            onChanged: (e) => print(e),
-          ),
-          const Text("Show password"),
-          TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('Back to login')),
-          TextButton(
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Home())),
-              child: const Text('Create account')),
-        ]))));
+                child: Container(
+      padding: const EdgeInsets.all(15),
+      margin: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Theme.of(context).colorScheme.tertiaryContainer,
+      ),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Image(
+              image: AssetImage('images/splash_screen.png'),
+              height: 50,
+            ),
+            const SizedBox(height: 20.0),
+            const Text(
+              "Create AutoMaat-account",
+              style: TextStyle(fontSize: 24),
+              textAlign: TextAlign.end,
+            ),
+            const Text("Register your account"),
+            const SizedBox(height: 20.0),
+            TextField(
+              controller: _nameFieldController,
+              decoration: const InputDecoration(
+                labelText: 'Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            TextField(
+              controller: _emailFieldController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            TextField(
+              controller: _passwordFieldController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            TextField(
+              controller: _passwordRepeatFieldController,
+              decoration: const InputDecoration(
+                labelText: 'Repeat password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Row(
+              children: [
+                TextButton(onPressed: () => Navigator.pop(context), child: const Text('Back to login')),
+                const Spacer(),
+                ElevatedButton(
+                    onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const Home())),
+                    child: const Text('Create account')),
+              ],
+            ),
+          ]),
+    ))));
   }
 }
