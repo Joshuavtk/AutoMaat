dc9a51bdece3bc1aa19380e7aa7feead0b80d294
Remko5 <r.m.vijfschagt@st.hanze.nl>
2025-01-20 15:24:35 +0100

    added red circle to current position on the map

--- lib/database/table/user.dart
+++ lib/database/table/user.dart
@@ -3,5 +3,6 @@ import 'package:drift_flutter/drift_flutter.dart';
 
 class User extends Table {
   IntColumn get id => integer().autoIncrement()();
+  TextColumn get username =>  text()();
   TextColumn get token => text()();
 }
--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -22,9 +22,10 @@ class MapWidget extends State<Map> {
   PermissionStatus? _permissionGranted;
   LocationData? _locationData;
   List<LatLng> carLocations = [];
-  var beginLat = 53.21917;
-  var beginLong = 6.56667;
-
+  var beginLat = 53.238316;
+  var beginLong = 6.53497;
+  double currentZoom = 16.0;
+  LatLng currentCenter = const LatLng(53.238316, 6.53497);
   @override
   void initState() {
     super.initState();
@@ -52,12 +53,24 @@ class MapWidget extends State<Map> {
     _locationData = await location.getLocation();
     setState(() {
       log(_locationData.toString());
-      mapController.move(
-        //LatLng(_locationData?.latitude ?? beginLat, _locationData?.longitude ?? beginLong),
-        LatLng(beginLat, beginLong),
-        16,
+      beginLat = _locationData?.latitude ?? beginLat;
+      beginLong = _locationData?.longitude ?? beginLong;
+      currentCenter = LatLng(beginLat, beginLong);
+      mapController.move(  
+        currentCenter,
+        currentZoom,
       );
     });
+  }
+
+  void _zoomIn() {
+    currentZoom = currentZoom + 1;
+    mapController.move(currentCenter, currentZoom);
+  }
+
+  void _zoomOut() {
+    currentZoom = currentZoom - 1;
+    mapController.move(currentCenter, currentZoom);
   }
 
   @override
@@ -74,8 +87,23 @@ class MapWidget extends State<Map> {
           ],
         ),
         body: Stack(
+          alignment: AlignmentDirectional.bottomEnd,
           children: [
             _getMapWidget(),
+            Column(
+              children: [
+                FloatingActionButton(
+                  onPressed: _zoomIn,
+                  tooltip: 'ZoomIn',
+                  child: const Icon(Icons.add_circle_rounded),
+                ),
+                FloatingActionButton(
+                  onPressed: _zoomOut,
+                  tooltip: 'ZoomOut',
+                  child: const Icon(Icons.remove_circle_outline_rounded),
+                ),
+              ],
+            )
           ],
         ));
   }
@@ -89,6 +117,7 @@ class MapWidget extends State<Map> {
         children: [
           _getMapLayer(),
           _getCarPointersLayer(),
+          _getCurrentPositionLayer(),
         ]);
   }
 
@@ -101,16 +130,30 @@ class MapWidget extends State<Map> {
 
   Widget _getCarPointersLayer() {
     print("carLocations: $carLocations");
-    
+
     return CircleLayer(
       circles: [
-        for(LatLng carLocation in carLocations) ...[
+        for (LatLng carLocation in carLocations) ...[
           CircleMarker(
-          point: carLocation,
-          radius: 30,
-          useRadiusInMeter: true,
-        ),
-      ]
+            point: carLocation,
+            radius: 30,
+            useRadiusInMeter: true,
+          ),
+        ]
+      ],
+    );
+  }
+
+  Widget _getCurrentPositionLayer() {
+    return CircleLayer(
+      circles: [
+          CircleMarker(
+            point: LatLng(beginLat, beginLong),      
+            radius: 30,
+            useRadiusInMeter: true,
+            color: const Color.fromRGBO(255, 0, 0, 1),
+            borderColor: const Color.fromRGBO(255, 0, 0, 1),
+          ),
       ],
     );
   }
@@ -121,6 +164,4 @@ class MapWidget extends State<Map> {
       carLocations = temp;
     });
   }
-
-  
 }
